b06a51e1654e1beadd3a731ca07146aea5328ff4
Bene SAYER <bentouster@gmail.com>
2025-04-23 12:04:52 +0200

    changed path of router and added '-' to cocktail-list, profile-list and profile favourites

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,17 @@ import Nav from "./components/Nav/Nav";
 import "./App.css";
 
 function App() {
+	
+	
+	//* const [cocktailList, setCocktailList] = useState([]);
+
+	async function getCocktails (){
+	const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`");
+	const data = await response.json();
+	setCocktailList(data.cocktails)*//
+
+	
+}
 	return (
 		<>
 			<Header />
