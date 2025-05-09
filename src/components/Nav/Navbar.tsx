42fbb2356d3d09ffb4b9681e417c20fdf75e19bc
S1mon_G <simonguillo@gmail.com>
2025-05-09 13:40:35 +0200

    desktop and mobile navbar added

---
+++ src/components/Nav/Navbar.tsx
@@ -0,0 +1,48 @@
+import "./Navbar.css";
+import { NavLink } from "react-router";
+
+function Navbar() {
+	return (
+		<>
+			<nav className="menu-nav">
+				<NavLink to="/">
+					<img
+						src="./src/images/home-icon.png"
+						alt="icône de l'accueil"
+						id="nav-icon"
+					/>
+				</NavLink>
+				<NavLink to="/quiz">
+					<img
+						src="./src/images/quiz-icon.png"
+						alt="icône du quiz"
+						id="nav-icon"
+					/>
+				</NavLink>
+				<NavLink to="/chat">
+					<img
+						src="./src/images/chat-icon.png"
+						alt="icône du chat"
+						id="nav-icon"
+					/>
+				</NavLink>
+				<NavLink to="/profile-favourites">
+					<img
+						src="./src/images/favourite-icon.png"
+						alt="icône des favoris"
+						id="nav-icon"
+					/>
+				</NavLink>
+				<NavLink to="/cocktail-list">
+					<img
+						src="./src/images/cocktail-icon.png"
+						alt="icône de la liste de cocktail"
+						id="nav-icon"
+					/>
+				</NavLink>
+			</nav>
+		</>
+	);
+}
+
+export default Navbar;
