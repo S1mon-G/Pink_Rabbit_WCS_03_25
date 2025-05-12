16b92a96706b679251743da2b8de374c96ddf298
S1mon_G <simonguillo@gmail.com>
2025-05-12 13:32:52 +0200

    homepage and carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,27 @@
+import Swal from "sweetalert2";
+import RulesCarousel from "../components/RulesCarousel/RulesCarousel";
+
+Swal.fire({
+	title: "💘",
+	text: "L'amour c'est enivrant mais l'alcool est à consommer avec modération",
+	showDenyButton: true,
+	confirmButtonText: "J’ai plus de 18 ans 👌",
+	denyButtonText: "je suis trop jeune 😞",
+	allowOutsideClick: false,
+}).then((result) => {
+	if (result.isDenied) {
+		window.location.href = "https://www.google.com";
+	}
+});
+
 function Home() {
-	return <h1>Home la maison </h1>;
+	return (
+		<>
+			<h2>Bienvenue sur l'appli de rencontre pour les fans de mixologie 🍸 </h2>
+			<h2>Comment ça marche ?</h2>
+			<RulesCarousel />
+		</>
+	);
 }
 
 export default Home;
