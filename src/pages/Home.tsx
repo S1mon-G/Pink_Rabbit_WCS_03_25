88c7376ac9acc17ec9386a535b3e9ef874ac0f20
S1mon_G <simonguillo@gmail.com>
2025-05-13 12:26:27 +0200

    modified alert so it appears only once

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,27 +1,42 @@
+import { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-
 import Swal from "sweetalert2";
 import RulesCarousel from "../components/RulesCarousel/RulesCarousel";
 
 function Home() {
-	Swal.fire({
-		title: "💘",
-		text: "L'amour c'est enivrant mais l'alcool est à consommer avec modération",
-		showDenyButton: true,
-		confirmButtonText: "J’ai plus de 18 ans 👌",
-		denyButtonText: "je suis trop jeune 😞",
-		allowOutsideClick: false,
-		allowEscapeKey: false,
-		focusConfirm: false,
-		background: "linear-gradient(#f2059f, #f20505)",
-		color: "#ffff",
-	}).then((result) => {
-		if (result.isDenied) {
-			window.location.href = "https://www.google.com";
-		}
-	});
-
+	const [hasOpenedAlert, setHasOpenedAlert] = useState(
+		sessionStorage.getItem("hasAlertOpened") === "true",
+	);
 	const navigate = useNavigate();
+
+	const handleAlert = useCallback(() => {
+		if (hasOpenedAlert) return;
+		setHasOpenedAlert(true);
+
+		Swal.fire({
+			title: "💘",
+			text: "L'amour c'est enivrant mais l'alcool est à consommer avec modération",
+			showDenyButton: true,
+			confirmButtonText: "J’ai plus de 18 ans 👌",
+			denyButtonText: "je suis trop jeune 😞",
+			allowOutsideClick: false,
+			allowEscapeKey: false,
+			focusConfirm: false,
+			background: "linear-gradient(#f2059f, #f20505)",
+			color: "#ffff",
+		}).then((result) => {
+			if (result.isDenied) {
+				window.location.href = "https://www.google.com";
+			}
+			sessionStorage.setItem("hasAlertOpened", "true");
+			setHasOpenedAlert(true);
+		});
+	}, [hasOpenedAlert]);
+
+	useEffect(() => {
+		handleAlert();
+	}, [handleAlert]);
+
 	return (
 		<>
 			<h2>Bienvenue sur l'appli de rencontre pour les fans de mixologie 🍸 </h2>
